4b41b56b8ed8c87244ee750a144cf1030f9322ad
hwalker928 <harry@hwal.uk>
2025-01-29 21:35:58 +0000

    add assets and remaining tasks

--- autodarts.sh
+++ autodarts.sh
@@ -15,14 +15,16 @@ sudo apt upgrade -y
 # Install the necessary packages
 sudo apt install curl rpi-connect -y
 
+# Enable VNC Server
 sudo raspi-config nonint do_vnc 1
 
+# Expand Root Filesystem
 sudo raspi-config nonint do_expand_rootfs
 
 # Copy the assets folder
 sudo cp -r assets/ ~/.assets/
 
-# TODO: change the path to the wallpaper
+# Set the wallpaper
 pcmanfm --set-wallpaper=~/.assets/wallpaper.jpg
 
 # Set the splash screen background
@@ -30,7 +32,7 @@ sudo cp assets/splash.png /usr/share/plymouth/themes/pix/splash.png
 sudo plymouth-set-default-theme -R pix
 
 # Copy the autodarts logo to the Raspberry Pi artwork directory
-sudo cp assets/autodarts_logo.png /usr/share/raspberrypi-artwork/autodarts_logo.png
+sudo cp assets/logo.png /usr/share/raspberrypi-artwork/autodarts_logo.png
 
 # Copy the login configuration file
 sudo cp configs/pi-greeter.conf /etc/lightdm/pi-greeter.conf
@@ -49,3 +51,11 @@ loginctl enable-linger
 
 rpi-connect signin
 rpi-connect on
+
+echo "Remaining tasks:"
+echo "1. Reboot the Raspberry Pi"
+echo "2. Disable Wastebasket and External Disks on the desktop"
+echo "3. Enable Desktop Autologin"
+echo "4. Set a static IP address"
+echo "5. Link the board to the account"
+echo "6. Configure the cameras"
